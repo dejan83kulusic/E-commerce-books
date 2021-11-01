51b49a6e5f841ae15c229334a62f1dc4f8869985
dejankulusic1983 <dejankulusic983@gmai.com>
2021-11-01 15:05:34 +0100

    first commit

---
+++ Spring-Boot-E-commmerce/src/main/java/com/dejan/SpringBootECommmerceApplication.java
@@ -0,0 +1,18 @@
+package com.dejan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class SpringBootECommmerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootECommmerceApplication.class, args);
+	}
+
+
+}
+
